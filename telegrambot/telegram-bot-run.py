c2ab084c60578154e5db11233b949ef4d530752a
Mamdasan Sabrian <mhsaber1375@gmail.com>
2023-11-03 10:05:44 +0100

    Restructure & remove redundant lines

--- telegrambot/telegram-bot-run.py
+++ telegrambot/telegram-bot-run.py
@@ -14,7 +14,7 @@ from telegram_bot_api import parse_message, \
 from threading import Thread
 import datetime
 from time import sleep
-from postgres_api import Fetchpostgres 
+from postgres_api import Fetchpostgres
 from postgresconf.config import config as pconfig
 from credentials import config
 
@@ -40,16 +40,17 @@ app = Flask(__name__)
 def manage_messages(msg):
     try:
         parsed_message = parse_message(msg)
-        chat_id, message_info, chat_type = parsed_message
-        if chat_type == 'callback_query':
-            handle_callback_query(chat_id, message_info)
-        elif chat_type == 'message':
-            handle_message(chat_id, message_info)
-        elif chat_type == 'inline_query':
-            handle_inline_query(inline_query_id=chat_id, message_info=message_info)
+        if parsed_message:
+            chat_id, message_info, chat_type = parsed_message
+            if chat_type == 'callback_query':
+                handle_callback_query(chat_id, message_info)
+            elif chat_type == 'private message':
+                handle_message(chat_id, message_info)
+            elif chat_type == 'inline_query':
+                handle_inline_query(inline_query_id=chat_id, message_info=message_info)
     except Exception as e:
         print(e)
-        
+
 def make_reply_markup_page_control(page_number, number_of_pages, command):
     reply_markup=''
     next_page = {'text': '>', 'callback_data': f'page {page_number+1} {command}'}
@@ -108,22 +109,22 @@ def handle_commands(message):
                 queries = fetcher.getBySpecificDate(date_query)
             except:
                 queries = ['This command should be used as follows:\n/date Day.Month.Year']
-        elif (message == '/help') or (message=='Help ❔'): 
+        elif (message == '/help') or (message=='Help ❔'):
             reply="""◾️What can this bot do?
 <b>/start</b>: start the bot
 <b>[query]</b>: get a list of protests with the query in their description.
 <b>/search [query]</b>: get a list of protests with the query in their description. (The same as the one above)
 For example: <i>'/search ukraine'</i> or <i>'ukraine'</i>
 <b>/today</b>: get a list of today's protests
-<b>/tomorrow</b>: get a list of tomorrow's protests 
-<b>/saturday</b>: get a list of the upcoming saturday's protests 
-<b>/week</b>: get a list of this week's protests 
-<b>/date [dd.mm.yyyy]</b>: get a list of protests on that specific date. 
+<b>/tomorrow</b>: get a list of tomorrow's protests
+<b>/saturday</b>: get a list of the upcoming saturday's protests
+<b>/week</b>: get a list of this week's protests
+<b>/date [dd.mm.yyyy]</b>: get a list of protests on that specific date.
 For example: <i>'/date 01.05.2030'</i>
 <b>/help</b>: see the manual
 <b>/info</b>: contact me
 With the following command you can search and select a specific protest info in any chat:
-<b>@ProtestsBerlinBot [query]</b>: get the protest records with the query in their description or if the query is a date, the records on that date. 
+<b>@ProtestsBerlinBot [query]</b>: get the protest records with the query in their description or if the query is a date, the records on that date.
 For example: <i>'@ProtestsBerlinBot ukraine'</i>"""
             queries = [reply]
 
@@ -137,14 +138,14 @@ For example: <i>'@ProtestsBerlinBot ukraine'</i>"""
             if not queries:
                 queries = ["There's nothing to show."]
     return queries, reply_markup
-    
+
 def handle_inline_query(inline_query_id, message_info):
     search_query = message_info.split(',')
     queries = fetcher.get_query_any_column(search_query, columns=['Aufzugsstrecke', 'Versammlungsort', 'Thema', 'PLZ', 'Datum'])
     results=[]
     if queries:
         for q in queries[:45]:
-            results.append({ 
+            results.append({
                     'type': 'article', \
                     'id' : f'{q[0]}', \
                     'title': f"{q[4]}", \
@@ -152,20 +153,20 @@ def handle_inline_query(inline_query_id, message_info):
                     'description': f"{q[1].strftime('%d.%m.%Y.') if q[1] else ''}{q[2].strftime('%H:%M')} {'to' if q[2] else '' } {q[3].strftime('%H:%M')} - {f'{q[5]}; {q[6]}' if q[5] else ''}{q[7] if q[7] else ''}", \
                     })
     else:
-        results.append({ 
+        results.append({
                     'type': 'article', \
                     'id' : '0', \
                     'title': "There's nothing to show.", \
                     'input_message_content': {'message_text': "There's nothing to show.", 'parse_mode': 'HTML'}, \
                     })
     r = answerInlineQuery(
-            inline_query_id=inline_query_id, 
+            inline_query_id=inline_query_id,
             results=results
             )
     print(r)
 
 def handle_callback_query(chat_id, message_info):
-    _, message_id, callback_query_data, callback_query_message_id, dice_value = message_info
+    _, message_id, callback_query_data, callback_query_message_id = message_info
     reply_markup = ''
     page_number = 1
     is_pagenumber_in_callback_query = callback_query_data.split()[0] == 'page'
@@ -177,10 +178,10 @@ def handle_callback_query(chat_id, message_info):
         queries, reply_markup = handle_commands(command)
     else:
         command = callback_query_data
-        queries, reply_markup = handle_commands(command) 
+        queries, reply_markup = handle_commands(command)
     print('callback_query_data:', callback_query_data)
     print('command:', command)
-    reply_markup_page, reply = message_and_reply_markup_format(page_number, queries, command) 
+    reply_markup_page, reply = message_and_reply_markup_format(page_number, queries, command)
     if reply_markup_page:
         reply_markup = reply_markup_page
     editMessageText(
@@ -191,17 +192,14 @@ def handle_callback_query(chat_id, message_info):
     )
 
 def handle_message(chat_id, message_info):
-    message, message_id, dice_value = message_info
-    queries, reply_markup_main = handle_commands(message) 
-    print('#'*20)
-    print(queries, reply_markup_main)
-    print('#'*20)
+    message, message_id = message_info
+    queries, reply_markup_main = handle_commands(message)
     # keyboard = [["/today", "/tomorrow"], ["/help", "/info"]]
     keyboard = [["Today 🪧", "Tomorrow 🪧", "🔎"],
                 ["This Week 🪧", "Weekend 🪧", "Calender 🗓️"],
                 ["Help ❔", "Info 💁"]]
     reply_keyboard_markup = {"keyboard": keyboard, "resize_keyboard": True, "input_field_placeholder": "Select one:"}
-    
+
     if len(queries)>0:
             reply_markup_page, page = message_and_reply_markup_format(page_number=1, queries=queries, command=message)
             reply_markup = reply_keyboard_markup
@@ -211,7 +209,7 @@ def handle_message(chat_id, message_info):
                 reply_markup = reply_markup_page
 
             r = send_message(
-                    chat_id=chat_id, 
+                    chat_id=chat_id,
                     text=page,
                     reply_to_message_id=message_id,
                     reply_markup=reply_markup
@@ -224,20 +222,6 @@ def handle_message(chat_id, message_info):
             reply_to_message_id=message_id,
             reply_markup=reply_keyboard_markup
         )
-#        sendVideo(
-#                chat_id=chat_id,
-#                message_id=message_id,
-#                fileaddress='help-telegram-bot.mp4',
-#                caption="""◾️What can this bot do?
-#<b>/start</b>: start the bot
-#<b>/search [search query]</b>: get the protests with the query in their description. For example: /search ukraine 
-#<b>/today</b>: get today's protests
-#<b>/tomorrow</b>: get tomorrow's protests 
-#<b>/date [dd.mm.yyyy]</b>: get the protests on that specific date. For example: /date 01.05.2030
-#<b>/help</b>: see the manual
-#<b>/info</b>: contact me
-#<b>@ProtestsBerlinBot [search query]</b>: get the protest records with the query in their description or if the query is a date, it returns the records on that date. For example @ProtestsBerlinBot ukraine"""
-#                )
     else:
         send_message(
             chat_id=chat_id,
@@ -245,7 +229,7 @@ def handle_message(chat_id, message_info):
             reply_to_message_id=message_id,
             reply_markup=reply_keyboard_markup
         )
-    
+
 @app.before_request
 def block_method():
     ip = str(request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
@@ -273,15 +257,14 @@ def index():
         f.write(msg_str)
         f.write('\n')
         f.close()
-        manage_messages(msg)
+    manage_messages(msg)
     return Response('Ok', status=200)
 
 def main():
     pass
- 
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
 
 
     app.run(host="0.0.0.0", port=5000)
-

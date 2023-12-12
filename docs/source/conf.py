d5a79531dd4e381422f642dc5ab4294cda1c01a7
Mamdasan S <mhsaber1375@gmail.com>
2023-12-12 17:22:28 +0100

    Sort the library imports by isort pre-commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import sys
 import os
+import sys
 
 # The 2nd parent dir of this current dir is the root dir
 cwd = os.getcwd()

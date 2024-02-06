9ee65b06950e7484f3d396c9ab63a3160a776d55
Mamdasan Sabrian <mhsaber1375@gmail.com>
2024-02-06 14:50:49 +0100

    Add methods to EventCrawler + Restructure
    + Update docstring + Apply pre-commits: :8ball:

--- protestcrawler/protestcrawler.py
+++ protestcrawler/protestcrawler.py
@@ -2,50 +2,54 @@ import asyncio
 import threading
 from queue import Queue
 from time import sleep
-from urllib.parse import urlparse
 
 from ProtestLibs import ProtestGrabber, ProtestPostgres
 
 
 class EventCrawler:
     """
-    A class for crawling events from specified URLs and processing them.
+    Crawls and processes events from specified URLs.
 
-    This class is designed to crawl event data from various websites, parse the information, and optionally store it in a database. It supports crawling from a set of predefined URLs, each with its own methods for fetching and parsing data.
+    This class is designed for crawling event data from various websites, parsing the obtained information, and, optionally, storing it in a database. It is capable of handling multiple sources by crawling from a set of predefined URLs, each requiring specific methods for data fetching and parsing. The class optimizes data retrieval and parsing through parallel processing.
 
-    Attributes:
-        | url (str): The URL from which the event data will be crawled.
-        | _supported_methods (dict): A dictionary mapping supported URLs to their respective crawling, parsing, and database-writing methods.
+    :param url: The URL from which to crawl events.
+    :type url: str
+    :param data_grabber: An instance of a data grabbing class with methods for fetching and parsing event data.
+    :type data_grabber: Callable
+    :param postgres_worker: An instance of a class responsible for writing data to a PostgreSQL database.
+    :type postgres_worker: Callable
 
+    :Example:
 
-    Methods:
-        | crawl(url, number_of_threads=1, save_to_database=True, \\*\\*kwargs): Main method to start the crawling process for a given URL.
-        | _run_in_parallel(parser, data, number_of_threads, \\*\\*kwargs): Static method to parse data in parallel using threading.
-        | _is_supported(): Checks if the provided URL is supported based on _supported_methods.
-        | _url_base(): Property that returns the base hostname of the provided URL.
+    .. code-block:: python
 
+        berlinde_url = "https://www.berlin.de/polizei/service/versammlungsbehoerde/versammlungen-aufzuege"
+        ecrawler = EventCrawler(berlinde_url, ProtestGrabber, ProtestPostgres)
 
-    The class provides a high-level interface for crawling events from various sources, parsing the details, and optionally storing them in a database. It handles parallel processing to optimize data retrieval and parsing.
+        # To crawl data
+        data = ecrawler.crawl(number_of_threads=8, save_to_database=True)
+        print(f"Number of protests: {len(data)}")
 
-    Example Usage:
-        | crawler = EventCrawler(supported_methods)
-        | crawled_data = crawler.crawl("http://example.com/events", number_of_threads=5)
+    .. note:: This class provides a high-level interface for event data crawling, parsing, and optional database storage.
 
+    .. warning:: Ensure that the `data_grabber` and `postgres_worker` classes are correctly implemented to avoid runtime errors.
     """
 
-    def __init__(self, methods):
-        self.url = None
-        self._supported_methods = methods
+    def __init__(self, url, data_grabber, postgres_worker):
+        self.url = url
+        self.data_grabber = data_grabber()
+        self.postgres_worker = postgres_worker()
+
+    def get_event_list(self):
+        return asyncio.run(self.data_grabber.get_protest_list(self.url))
 
     @property
-    def _url_base(self):
-        return urlparse(self.url).hostname
+    def parse_event_list(self):
+        return self.data_grabber.parse_protest_list
 
-    def _is_supported(self):
-        if self._url_base in self._supported_methods:
-            return True
-        else:
-            return False
+    @property
+    def write_to_database(self):
+        return self.postgres_worker.write_to_database
 
     @staticmethod
     def _run_in_parallel(parser, data, number_of_threads, **kwargs):
@@ -70,32 +74,16 @@ class EventCrawler:
         queue.join()
         return output_data
 
-    def crawl(self, url, number_of_threads=1, save_to_database=True, **kwargs):
-        self.url = url
-        if not self._is_supported():
-            raise ValueError(
-                f"This url is not supported!\nPlease include the internet protocol,e.g. http https, "
-                f"in url.\nSupported urls: {[k for k in self._supported_methods]}"
-            )
-            pass
-
-        get_event_list = self._supported_methods[f"{self._url_base}"]["get_event_list"]
-        parse_event_list = self._supported_methods[f"{self._url_base}"][
-            "parse_event_list"
-        ]
-
-        event_list = asyncio.run(get_event_list(self.url))
+    def crawl(self, number_of_threads=1, save_to_database=True, **kwargs):
+        event_list = self.get_event_list()
         concurrent_threads = number_of_threads
         crawled_data = self._run_in_parallel(
-            parse_event_list, event_list, concurrent_threads, **kwargs
+            self.parse_event_list, event_list, concurrent_threads, **kwargs
         )
 
         if save_to_database:
             print("Writing/Updating data in database")
-            write_to_database = self._supported_methods[f"{self._url_base}"][
-                "write_to_database"
-            ]
-            write_response = write_to_database(crawled_data)
+            write_response = self.write_to_database(crawled_data)
             if write_response:
                 print("Writing/Updating data in database is done.")
             else:
@@ -104,23 +92,17 @@ class EventCrawler:
         return crawled_data
 
 
-# Define a method
-method = {
-    "www.berlin.de": {
-        "get_event_list": ProtestGrabber.get_protest_list,
-        "parse_event_list": ProtestGrabber.parse_protest_list,
-        "write_to_database": ProtestPostgres.write_to_database,
-    }
-}
-ecrawler = EventCrawler(method)
+berlinde_url = (
+    "https://www.berlin.de/polizei/service/versammlungsbehoerde/versammlungen-aufzuege"
+)
+
+ecrawler = EventCrawler(berlinde_url, ProtestGrabber, ProtestPostgres)
 
 if __name__ == "__main__":
     while True:
         try:
             print("Scraping data from berlin.de")
-            url = "https://www.berlin.de/polizei/service/versammlungsbehoerde/versammlungen-aufzuege"
             data = ecrawler.crawl(
-                url,
                 number_of_threads=8,
                 save_to_database=True,
             )

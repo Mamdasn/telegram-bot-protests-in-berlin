86d7b506016e965a9bd2674856e242aa80c59bd2
Mamdasan Sabrian <mhsaber1375@gmail.com>
2025-04-03 02:26:37 +0200

    Move entrypoint sh files out of telegrambot dir

--- telegrambot-entrypoint/nginx-setup.sh
+++ telegrambot-entrypoint/nginx-setup.sh
@@ -1,11 +1,35 @@
 #!/bin/sh
 
 echo Setting up the nginx configuration file
-cp /app/initial-setup-files/portforwarding.conf /etc/nginx/nginx.conf
+cat > /etc/nginx/nginx.conf <<EOF
+events {
+    worker_connections 1000;
+}
+http {
+    server {
+	listen 443 ssl;
+        server_name localhost;
+        ssl_certificate /etc/nginx/cert.pem;
+        ssl_certificate_key /etc/nginx/cert.key;
+        
+        location / {
+WHITE_LISTED_IPS
+		deny all;
+    		proxy_redirect off;
+        	proxy_pass http://0.0.0.0:5000;
+		proxy_set_header Host \\\$http_host;
+    		proxy_set_header X-Scheme \\\$scheme;
+    		proxy_set_header X-Real-IP \\\$remote_addr;
+    		proxy_set_header X-Forwarded-For \\\$proxy_add_x_forwarded_for;
+        }
+    }
+}
+EOF
+
 
 echo Copying the ssl file to the /etc/nginx dir
-SSL_PUBLIC=$(realpath SSLFILES/YOURPUBLIC.pem)
-SSL_PRIVATE=$(realpath SSLFILES/YOURPRIVATE.key)
+SSL_PUBLIC=/app/SSLFILES/YOURPUBLIC.pem
+SSL_PRIVATE=/app/SSLFILES/YOURPRIVATE.key
 cp "$SSL_PUBLIC" /etc/nginx/cert.pem
 cp "$SSL_PRIVATE" /etc/nginx/cert.key
 
